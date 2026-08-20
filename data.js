a54ffc28d1599c559f2264dabc0f035f9edf2f3a
auvry7578-png <auvry7578@gmail.com>
2026-08-21 08:17:16 +0900

    Auto publish: data.js

--- data.js
+++ data.js
@@ -1,4 +1,4 @@
-window.PUBLIC_PORTAL_META = {"generated_at": "2026-08-20 08:09", "source_note": "대한민국 정책브리핑 및 중앙부처 공식 보도자료"};
+window.PUBLIC_PORTAL_META = {"generated_at": "2026-08-21 08:17", "source_note": "대한민국 정책브리핑 및 중앙부처 공식 보도자료"};
 window.PUBLIC_DATA = [
   {
     "id": 7,
